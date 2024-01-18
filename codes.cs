221238d5a067ff971a79548532c869855ff90ceb
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2024-01-18 22:03:49 +0530

    Create codes.cs

---
+++ codes.cs
@@ -0,0 +1,8 @@
+- using System - just like import in other programming languages
+- using namespace - to bundle classes under same file
+
+- Console.WriteLine() - enters on new line
+- Console.Write() - enters on same line
+
+- //single line comment
+- /* */  multi line comment
